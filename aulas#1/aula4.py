05c17014a7aaaf5bae74a20d1d2fdea64aa32811
under <stevenfadublagem@gmail.com>
2024-07-30 15:49:38 -0300

    (UPDATE ESTUDO 10)

--- aulas#1/aula4.py
+++ aulas#1/aula4.py
@@ -31,3 +31,35 @@ else:
     print('E o preço da sua passagem será de R${}'.format(pre2))
 
 # atividade 34
+from datetime import date
+ano = int(input('Que ano quer analisar? Coloque 0 para analisar o ano atual: '))
+
+if ano == 0:
+    ano = date.today().year
+    
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano {} é BISSEXTO'.format(ano))
+else:
+    print('O ano {} NÃO é BISSEXTO'.format(ano))
+
+# atividade 35
+p1 = int(input('Primeiro valor: '))
+p2 = int(input('Segundo valor: '))
+p3 = int(input('Terceiro valor: '))
+
+meno = p1
+if p2<p1 and p2<p3:
+    menor = p2
+if p3<p1 and p3<p2:
+    menor = p3
+
+maior = p1
+if p2>p1 and p2>p3:
+    maior = p2
+if p3>p1 and p3>p2:
+    maior = p3
+
+print('O menor valor digitado foi {}'.format(menor))
+print('O maior valor digitado foi {}'.format(maior))
+
+# atividade 36
